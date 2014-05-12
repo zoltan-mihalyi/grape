91a4e43e349485baa5a745be9a1fd3619fff4440
mizraai <mizraai@pc1W18.inf.elte.hu>
2014-05-12 17:52:25 +0200

    requestAnimationFrame, examples readme

---
+++ examples/pong/README.md
@@ -0,0 +1,20 @@
+This folder includes multiple version of the Grape pong game.
+
+Folders
+-------
+
+- one-file: If you are developing a small game, and you don't need any module loader.
+- required: If you have a lot of modules, it is recommended to use require.js.
+- multiplayer: If you want to create a multiplayer game, you can see how easy it is.
+- audio, img: Common resource files are stored here.
+
+General
+-------
+
+In each directopry there are similar files.
+
+- index.html: This is the production version of the game, you should create something similar when you release your game. To use this, you have to build the game (see below). 
+- index.dev.html: If you are developing the game and the game engine is built, you use something like this.
+- index.grape-dev.html: If you are developing the game engine itself, you can use this. It works without building anything.
+
+TODO how to build?
--- js/grape/game/game-loop.js
+++ js/grape/game/game-loop.js
@@ -1,4 +1,47 @@
 define(['class'], function (Class) {
+    var reqTimeout,clearReqTimeout, reqInterval, clearReqInterval;
+    if (typeof window!=='undefined') { //todo env.browser
+        reqTimeout = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            window.mozRequestAnimationFrame;
+    
+        clearReqTimeout = window.cancelAnimationFrame ||
+            window.webkitCancelAnimationFrame ||
+            window.oCancelAnimationFrame ||
+            window.msCancelAnimationFrame ||
+            window.mozCancelRequestAnimationFrame;
+    }
+    
+    if(reqTimeout){ //we have native requestAnimationFrame
+        reqInterval = function(callback){
+            var handle;
+            function run(){
+                callback();
+                handle = reqTimeout(run);
+            }
+            handle = reqTimeout(run);
+            return {
+                _stop:function(){
+                    clearReqTimeout(handle);
+                }
+            };
+        };
+        
+        clearReqInterval=function(handle){
+            handle._stop();
+        };
+    } else { //we have to use a polyfill
+        reqTimeout = function(callback){
+            return setTimeout (callback, 16);
+        };
+        
+        reqInterval=function(callback){
+            return setInterval(callback, 16);
+        };
+    }
+    
     var now = Date.now ? Date.now : function () {
         return new Date().getTime();
     };
@@ -13,7 +56,7 @@ define(['class'], function (Class) {
             var backlog = 0;
             var last = now();
             var lastRenderStart = last;
-            this.intervalId = setInterval(function () {
+            this.intervalId = reqInterval(function () {
                 currentGame = game;
 
                 var start = now(), wasFrame = false;
@@ -38,7 +81,7 @@ define(['class'], function (Class) {
             if (!this.isRunning()) {
                 throw 'not running';
             }
-            clearInterval(this.intervalId);
+            clearReqInterval(this.intervalId);
             this.intervalId = null;
         },
         isRunning: function () {
